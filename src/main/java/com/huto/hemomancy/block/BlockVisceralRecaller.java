4120931a8ffcbe50f3324a3665f118a404a998be
Vincent Huto <amae804@gmail.com>
2021-05-08 01:06:26 -0500

    Remodled V.A.R

--- src/main/java/com/huto/hemomancy/block/BlockVisceralRecaller.java
+++ src/main/java/com/huto/hemomancy/block/BlockVisceralRecaller.java
@@ -2,7 +2,6 @@ package com.huto.hemomancy.block;
 
 import java.util.stream.Stream;
 
-import com.huto.hemomancy.tile.TileEntityMortalDisplay;
 import com.huto.hemomancy.tile.TileEntityVisceralRecaller;
 
 import net.minecraft.block.Block;
@@ -30,43 +29,36 @@ import net.minecraft.world.World;
 
 public class BlockVisceralRecaller extends Block {
 	public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
-	private static final VoxelShape SHAPE_N = Stream.of(Block.makeCuboidShape(1.5, 9.15, 1.5, 14.5, 14.65, 14.5),
-			Block.makeCuboidShape(3.25, 14.500000000000002, 3.25, 12.75, 18.5, 12.5),
-			Block.makeCuboidShape(1.25, 11.95, 1.25, 14.75, 14.2, 14.75),
-			Block.makeCuboidShape(2.75, 14.500000000000002, 2.75, 13.25, 18, 12.75),
-			Block.makeCuboidShape(12.3, 0, 12.3, 15.2, 1, 15.2),
-			Block.makeCuboidShape(12.5, 0.09999999999999964, 12.5, 15, 11.100000000000001, 15),
-			Block.makeCuboidShape(12.5, 0.09999999999999964, 1, 15, 11.100000000000001, 3.5),
-			Block.makeCuboidShape(13.25, 2.0999999999999996, 3.5, 14.25, 3, 12.5),
-			Block.makeCuboidShape(13.25, 8.1, 3.5, 14.25, 9, 12.5),
-			Block.makeCuboidShape(12.3, 0, 0.8000000000000007, 15.2, 1, 3.6999999999999993),
-			Block.makeCuboidShape(2.5, 2.0999999999999996, 13.25, 12.5, 3, 14.25),
-			Block.makeCuboidShape(2.5, 8.1, 13.25, 12.5, 9, 14.25),
-			Block.makeCuboidShape(3.3500000000000014, 13.300000000000004, 12.550000000000002, 12.45, 17,
-					12.550000000000002),
-			Block.makeCuboidShape(12.050000000000002, 13.3, 3.45, 12.050000000000002, 17, 12.55),
-			Block.makeCuboidShape(3.3500000000000014, 11.500000000000002, 3.850000000000002, 12.45, 15.2,
-					3.850000000000002),
-			Block.makeCuboidShape(4.249999999999998, 11.550000000000002, 3.45, 4.249999999999998, 15.25, 12.55),
-			Block.makeCuboidShape(13.25, 5.1, 3.5, 14.25, 6, 12.5),
-			Block.makeCuboidShape(0.75, 0.09999999999999964, 12.5, 3.25, 11.100000000000001, 15),
-			Block.makeCuboidShape(0.5500000000000007, 0, 12.3, 3.4499999999999993, 1, 15.2),
-			Block.makeCuboidShape(0.75, 0.09999999999999964, 0.75, 3.25, 11.100000000000001, 3.25),
-			Block.makeCuboidShape(0.5500000000000007, 0, 0.5500000000000007, 3.4499999999999993, 1, 3.4499999999999993),
-			Block.makeCuboidShape(2.5, 5.1, 13.25, 12.5, 6, 14.25), Block.makeCuboidShape(2.5, 5.1, 1.5, 12.5, 6, 2.5),
-			Block.makeCuboidShape(2.5, 2.0999999999999996, 1.5, 12.5, 3, 2.5),
-			Block.makeCuboidShape(2.5, 8.1, 1.5, 12.5, 9, 2.5),
-			Block.makeCuboidShape(1.25, 2.0999999999999996, 3.5, 2.25, 3, 12.5),
-			Block.makeCuboidShape(1.25, 5.1, 3.5, 2.25, 6, 12.5), Block.makeCuboidShape(1.25, 8.1, 3.5, 2.25, 9, 12.5),
-			Block.makeCuboidShape(12.3, 11, 12.3, 15.2, 12, 15.2),
-			Block.makeCuboidShape(12.3, 11, 0.8000000000000007, 15.2, 12, 3.6999999999999993),
-			Block.makeCuboidShape(0.5500000000000007, 11, 12.3, 3.4499999999999993, 12, 15.2),
-			Block.makeCuboidShape(0.5500000000000007, 11, 0.5500000000000007, 3.4499999999999993, 12,
-					3.4499999999999993),
-			Block.makeCuboidShape(6.5, 0.09999999999999964, 6.5, 9, 11.100000000000001, 9),
-			Block.makeCuboidShape(6.300000000000001, 0, 6.300000000000001, 9.2, 1, 9.2),
-			Block.makeCuboidShape(6.300000000000001, 8.1, 6.300000000000001, 9.2, 9.1, 9.2),
-			Block.makeCuboidShape(1.25, 9.2, 1.25, 14.75, 11.45, 14.75)).reduce((v1, v2) -> {
+	private static final VoxelShape SHAPE_N = Stream.of(Block.makeCuboidShape(1.5, 2, 3.25, 2.5, 3, 12.75),
+			Block.makeCuboidShape(13.5, 7, 3, 14.5, 8, 12.75), Block.makeCuboidShape(13.5, 9, 3, 14.5, 10, 12.75),
+			Block.makeCuboidShape(0.75, 0.1, 0.75, 3.25, 14.1, 3.25), Block.makeCuboidShape(0.5, 0, 0.5, 3.5, 1, 3.5),
+			Block.makeCuboidShape(0.5, 13.5, 0.5, 3.5, 14.5, 3.5),
+			Block.makeCuboidShape(12.75, 0.1, 0.75, 15.25, 14.1, 3.25),
+			Block.makeCuboidShape(12.5, 0, 0.5, 15.5, 1, 3.5), Block.makeCuboidShape(12.5, 13.5, 0.5, 15.5, 14.5, 3.5),
+			Block.makeCuboidShape(0.75, 0.1, 12.75, 3.25, 14.1, 15.25),
+			Block.makeCuboidShape(0.5, 0, 12.5, 3.5, 1, 15.5), Block.makeCuboidShape(0.5, 13.5, 12.5, 3.5, 14.5, 15.5),
+			Block.makeCuboidShape(12.75, 0.1, 12.75, 15.25, 14, 15.25),
+			Block.makeCuboidShape(12.5, 0, 12.5, 15.5, 1, 15.5),
+			Block.makeCuboidShape(12.5, 13.5, 12.5, 15.5, 14.5, 15.5),
+			Block.makeCuboidShape(13.5, 4, 3, 14.5, 5, 12.75), Block.makeCuboidShape(13.5, 2, 3, 14.5, 3, 12.75),
+			Block.makeCuboidShape(1.5, 4, 3.25, 2.5, 5, 12.75), Block.makeCuboidShape(1.5, 7, 3.25, 2.5, 8, 12.75),
+			Block.makeCuboidShape(1.5, 9, 3.25, 2.5, 10, 12.75), Block.makeCuboidShape(3.25, 7, 1.5, 12.75, 8, 2.5),
+			Block.makeCuboidShape(3.25, 9, 1.5, 12.75, 10, 2.5), Block.makeCuboidShape(3.25, 4, 1.5, 12.75, 5, 2.5),
+			Block.makeCuboidShape(3.25, 2, 1.5, 12.75, 3, 2.5), Block.makeCuboidShape(3.25, 2, 13.5, 12.75, 3, 14.5),
+			Block.makeCuboidShape(3.25, 4, 13.5, 12.75, 5, 14.5), Block.makeCuboidShape(3.25, 7, 13.5, 12.75, 8, 14.5),
+			Block.makeCuboidShape(3.25, 9, 13.5, 12.75, 10, 14.5), Block.makeCuboidShape(2, 7, 2, 14, 11, 14),
+			Block.makeCuboidShape(4.75, 11.25, 4.75, 11.25, 17.25, 11.25),
+			Block.makeCuboidShape(1, 12.25, 1, 15, 14.25, 15), Block.makeCuboidShape(-1, 11.75, -1, 2, 14.75, 2),
+			Block.makeCuboidShape(-1, 13.35, 2, 1, 15.35, 14), Block.makeCuboidShape(-1, 11.75, 14, 2, 14.75, 17),
+			Block.makeCuboidShape(2, 13.35, 15, 14, 15.35, 17), Block.makeCuboidShape(14, 11.75, 14, 17, 14.75, 17),
+			Block.makeCuboidShape(15, 13.35, 2, 17, 15.35, 14), Block.makeCuboidShape(14, 11.75, -1, 17, 14.75, 2),
+			Block.makeCuboidShape(2, 13.35, -1, 14, 15.35, 1), Block.makeCuboidShape(0.5, 9.5, 0.5, 3.5, 12.5, 3.5),
+			Block.makeCuboidShape(0.5, 9.5, 12.5, 3.5, 12.5, 15.5),
+			Block.makeCuboidShape(12.5, 9.5, 12.5, 15.5, 12.5, 15.5),
+			Block.makeCuboidShape(12.5, 9.5, 0.5, 15.5, 12.5, 3.5),
+			Block.makeCuboidShape(12.5, 1.5, 0.5, 15.5, 4.5, 3.5), Block.makeCuboidShape(0.5, 1.5, 0.5, 3.5, 4.5, 3.5),
+			Block.makeCuboidShape(0.5, 1.5, 12.5, 3.5, 4.5, 15.5),
+			Block.makeCuboidShape(12.5, 1.5, 12.5, 15.5, 4.5, 15.5)).reduce((v1, v2) -> {
 				return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);
 			}).get();
 
