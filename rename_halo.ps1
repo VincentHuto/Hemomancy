fdd44bf9933d39c676b0cfe776169a01696171ad
Vincent Huto <amae804@gmail.com>
2026-04-15 23:00:49 -0500

    removed unneeded scripts

--- rename_halo.ps1
+++
@@ -1,8 +0,0 @@
-$src = "C:\Users\Vince\Desktop\My MC Mods\CURRENT\Hemomancy\src\main\resources\assets\hemomancy\textures\item\scars\rune_halo.png"
-$dst = "C:\Users\Vince\Desktop\My MC Mods\CURRENT\Hemomancy\src\main\resources\assets\hemomancy\textures\item\scars\scar_halo.png"
-if (Test-Path $src) {
-    Move-Item $src $dst -Force
-    Write-Host "Renamed rune_halo.png to scar_halo.png"
-} else {
-    Write-Host "rune_halo.png not found (may already be renamed)"
-}
