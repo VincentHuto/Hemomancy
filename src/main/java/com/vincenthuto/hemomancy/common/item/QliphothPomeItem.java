3e9e431e6f3b90f370c3936d49edfa5ac41d36af
Vincent Huto <amae804@gmail.com>
2026-04-28 13:12:37 -0500

    Change pome eat message

--- src/main/java/com/vincenthuto/hemomancy/common/item/QliphothPomeItem.java
+++ src/main/java/com/vincenthuto/hemomancy/common/item/QliphothPomeItem.java
@@ -133,16 +133,16 @@ public class QliphothPomeItem extends Item implements HemoClientItemExtensionsPr
 				int huskIdx = tag.getInt(HUSK_INDEX_KEY);
 				if (huskIdx >= 0 && huskIdx < HUSK_NAMES.length) {
 					int ordinal = huskIdx + 1;
-					tooltip.add(Component.literal(ordinalString(ordinal) + " of nine. Sweet in the way that the color black is sweet.")
+					tooltip.add(Component.literal(ordinalString(ordinal) + " of nine. Sweet in the way that tv static and silence is sweet.")
 							.withStyle(ChatFormatting.DARK_PURPLE, ChatFormatting.ITALIC));
 					tooltip.add(Component.literal("[" + HUSK_NAMES[huskIdx] + "]")
 							.withStyle(ChatFormatting.DARK_PURPLE));
 				} else {
-					tooltip.add(Component.literal("One of nine. Sweet in the way that the color black is sweet.")
+					tooltip.add(Component.literal("One of nine. Sweet in the way that tv static and silence is sweet.")
 							.withStyle(ChatFormatting.DARK_PURPLE, ChatFormatting.ITALIC));
 				}
 			} else {
-				tooltip.add(Component.literal("One of nine. Sweet in the way that the color black is sweet.")
+				tooltip.add(Component.literal("One of nine. Sweet in the way that tv static and silence is sweet.")
 						.withStyle(ChatFormatting.DARK_PURPLE, ChatFormatting.ITALIC));
 			}
 			tooltip.add(Component.literal("+300 Blood Volume")
