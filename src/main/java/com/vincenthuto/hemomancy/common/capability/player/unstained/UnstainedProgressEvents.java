81ae4da52a5dc8082f19179a7b2ee13fbb1e98b4
Vincent Huto <amae804@gmail.com>
2026-05-03 18:52:30 -0500

    Fixed smelting recipes

--- src/main/java/com/vincenthuto/hemomancy/common/capability/player/unstained/UnstainedProgressEvents.java
+++ src/main/java/com/vincenthuto/hemomancy/common/capability/player/unstained/UnstainedProgressEvents.java
@@ -3,19 +3,38 @@ package com.vincenthuto.hemomancy.common.capability.player.unstained;
 import net.neoforged.fml.common.EventBusSubscriber;
 import com.vincenthuto.hemomancy.common.capability.HemoCapabilityAccess;
 import com.vincenthuto.hemomancy.Hemomancy;
+import com.vincenthuto.hemomancy.common.init.ItemInit;
 import com.vincenthuto.hemomancy.common.network.PacketHandler;
 import com.vincenthuto.hemomancy.common.network.capa.PacketSyncUnstainedProgress;
 
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent.PlayerChangedDimensionEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent.PlayerRespawnEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.network.PacketDistributor;
 
 @EventBusSubscriber(modid = Hemomancy.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
 public class UnstainedProgressEvents {
+
+    public static void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(UnstainedProgressEvents::initUnstainedStageIcons);
+    }
+
+    private static void initUnstainedStageIcons() {
+        EnumPurityStage.CORRUPTED.setIconItem(() -> new ItemStack(ItemInit.blood_stained_stone.get()));
+        EnumPurityStage.TAINTED.setIconItem(() -> new ItemStack(ItemInit.lethean_poppy_wreath.get()));
+        EnumPurityStage.CLEANSING.setIconItem(() -> new ItemStack(ItemInit.pale_distillate.get()));
+        EnumPurityStage.ABSOLVED.setIconItem(() -> new ItemStack(ItemInit.tears_of_silthmere.get()));
+        EnumPurityStage.PURIFIED.setIconItem(() -> new ItemStack(ItemInit.pallid_icon.get()));
+        EnumClarityStage.AWAKENED.setIconItem(() -> new ItemStack(ItemInit.cleansed_blood_crystal_shard.get()));
+        EnumClarityStage.DISCERNING.setIconItem(() -> new ItemStack(ItemInit.silver_chalice.get()));
+        EnumClarityStage.VIGILANT.setIconItem(() -> new ItemStack(ItemInit.pale_silver_ingot.get()));
+        EnumClarityStage.RESOLUTE.setIconItem(() -> new ItemStack(ItemInit.tome_of_the_unstained.get()));
+        EnumClarityStage.ENLIGHTENED.setIconItem(() -> new ItemStack(ItemInit.pallid_icon.get()));
+    }
 
     public static void syncProgress(ServerPlayer player, IUnstainedProgress progress) {
         PacketHandler.sendToPlayer(player, new PacketSyncUnstainedProgress(progress));
