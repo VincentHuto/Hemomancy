a3e5b846c58425e99b9f4a151bc09e85e620ca5b
Vincent Huto <amae804@gmail.com>
2026-06-19 23:21:16 -0500

    Updated HematicArmatureModel with vicarkit_upgrade and monolithic_cornerstone sections for upgrading

--- src/main/java/com/vincenthuto/hemomancy/client/model/tile/crafting/HematicArmatureModel.java
+++ src/main/java/com/vincenthuto/hemomancy/client/model/tile/crafting/HematicArmatureModel.java
@@ -24,19 +24,20 @@ public class HematicArmatureModel extends Model {
 	private final ModelPart left_pillar;
 	private final ModelPart right_pillar;
 	private final ModelPart arch;
-	private final ModelPart apex;
+	private final ModelPart monolithic_cornerstone;
 	private final ModelPart left_vial;
 	private final ModelPart right_vial;
-	private final ModelPart banners;
-	private final ModelPart bannerL;
-	private final ModelPart bannerR;
 	private final ModelPart chains;
 	private final ModelPart bowls;
 	private final ModelPart altar_outer_left;
 	private final ModelPart altar_outer_right;
 	private final ModelPart altar_inner_left;
 	private final ModelPart altar_inner_right;
-
+	private final ModelPart vicarkit_upgrade;
+	private final ModelPart candles;
+	private final ModelPart banners;
+	private final ModelPart bannerL;
+	private final ModelPart bannerR;
 
 	public HematicArmatureModel(ModelPart root) {
 		super(RenderType::entityTranslucent);
@@ -44,20 +45,21 @@ public class HematicArmatureModel extends Model {
 		this.left_pillar = root.getChild("left_pillar");
 		this.right_pillar = root.getChild("right_pillar");
 		this.arch = root.getChild("arch");
-		this.apex = root.getChild("apex");
+		this.monolithic_cornerstone = root.getChild("monolithic_cornerstone");
 		this.left_vial = root.getChild("left_vial");
 		this.right_vial = root.getChild("right_vial");
-		this.banners = root.getChild("banners");
-		this.bannerL = this.banners.getChild("bannerL");
-		this.bannerR = this.banners.getChild("bannerR");
 		this.chains = root.getChild("chains");
 		this.bowls = root.getChild("bowls");
 		this.altar_outer_left = this.bowls.getChild("altar_outer_left");
 		this.altar_outer_right = this.bowls.getChild("altar_outer_right");
 		this.altar_inner_left = this.bowls.getChild("altar_inner_left");
 		this.altar_inner_right = this.bowls.getChild("altar_inner_right");
+		this.vicarkit_upgrade = root.getChild("vicarkit_upgrade");
+		this.candles = this.vicarkit_upgrade.getChild("candles");
+		this.banners = this.vicarkit_upgrade.getChild("banners");
+		this.bannerL = this.banners.getChild("bannerL");
+		this.bannerR = this.banners.getChild("bannerR");
 	}
-
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
@@ -65,13 +67,7 @@ public class HematicArmatureModel extends Model {
 
 		PartDefinition platform = partdefinition.addOrReplaceChild("platform", CubeListBuilder.create().texOffs(0, 0).addBox(-22.0F, -2.0F, -22.0F, 44.0F, 2.0F, 44.0F, new CubeDeformation(0.0F))
 				.texOffs(0, 47).addBox(-12.0F, -5.0F, -10.0F, 24.0F, 4.0F, 20.0F, new CubeDeformation(0.0F))
-				.texOffs(92, 142).addBox(-5.0F, -8.0F, -5.0F, 10.0F, 3.0F, 10.0F, new CubeDeformation(0.0F))
-				.texOffs(127, 183).addBox(14.0F, -8.0F, -14.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F))
-				.texOffs(87, 192).addBox(16.0F, -5.0F, 12.0F, 2.0F, 5.0F, 2.0F, new CubeDeformation(0.0F))
-				.texOffs(39, 186).addBox(-16.0F, -8.0F, -14.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F))
-				.texOffs(192, 93).addBox(-18.0F, -5.0F, 12.0F, 2.0F, 5.0F, 2.0F, new CubeDeformation(0.0F))
-				.texOffs(192, 84).addBox(7.0F, -6.0F, -17.0F, 2.0F, 6.0F, 2.0F, new CubeDeformation(0.0F))
-				.texOffs(96, 192).addBox(-9.0F, -4.0F, -17.0F, 2.0F, 4.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 24.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
+				.texOffs(92, 142).addBox(-5.0F, -8.0F, -5.0F, 10.0F, 3.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 24.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
 
 		PartDefinition left_pillar = partdefinition.addOrReplaceChild("left_pillar", CubeListBuilder.create().texOffs(0, 157).addBox(-4.0F, -8.0F, -3.0F, 8.0F, 8.0F, 6.0F, new CubeDeformation(0.0F))
 				.texOffs(0, 91).addBox(-1.0F, -54.0F, -2.0F, 4.0F, 46.0F, 4.0F, new CubeDeformation(0.0F))
@@ -95,7 +91,7 @@ public class HematicArmatureModel extends Model {
 				.texOffs(175, 155).addBox(17.0F, 2.0F, -2.0F, 6.0F, 2.0F, 4.0F, new CubeDeformation(0.0F))
 				.texOffs(177, 26).addBox(-23.0F, 2.0F, -2.0F, 6.0F, 2.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -34.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
 
-		PartDefinition apex = partdefinition.addOrReplaceChild("apex", CubeListBuilder.create().texOffs(51, 142).addBox(-6.0F, -2.0F, -4.0F, 12.0F, 12.0F, 8.0F, new CubeDeformation(0.0F))
+		PartDefinition monolithic_cornerstone = partdefinition.addOrReplaceChild("monolithic_cornerstone", CubeListBuilder.create().texOffs(51, 142).addBox(-6.0F, -2.0F, -4.0F, 12.0F, 12.0F, 8.0F, new CubeDeformation(0.0F))
 				.texOffs(177, 0).addBox(5.9F, -2.0F, -1.0F, 8.0F, 10.0F, 2.0F, new CubeDeformation(0.0F))
 				.texOffs(177, 13).addBox(-13.9F, -2.0F, -1.0F, 8.0F, 10.0F, 2.0F, new CubeDeformation(0.0F))
 				.texOffs(177, 13).addBox(-3.9F, 10.0F, -1.0F, 8.0F, 6.0F, 2.0F, new CubeDeformation(0.0F))
@@ -115,16 +111,6 @@ public class HematicArmatureModel extends Model {
 		PartDefinition right_vial = partdefinition.addOrReplaceChild("right_vial", CubeListBuilder.create().texOffs(51, 184).addBox(-3.0F, -18.0F, -1.0F, 6.0F, 8.0F, 2.0F, new CubeDeformation(0.0F))
 				.texOffs(51, 184).addBox(-3.0F, -37.0F, -1.0F, 6.0F, 3.0F, 2.0F, new CubeDeformation(0.0F))
 				.texOffs(172, 110).addBox(-6.0F, -30.0F, -2.0F, 4.0F, 11.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-21.0F, 24.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
-
-		PartDefinition banners = partdefinition.addOrReplaceChild("banners", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 24.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
-
-		PartDefinition bannerL = banners.addOrReplaceChild("bannerL", CubeListBuilder.create().texOffs(179, 61).addBox(-4.5F, -2.0F, -0.5F, 10.0F, 4.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(151, 114).addBox(-4.5F, 2.0F, -0.5F, 9.0F, 24.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(187, 44).addBox(-4.5F, 26.0F, -0.5F, 8.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(28.5F, -48.0F, 0.0F));
-
-		PartDefinition bannerR = banners.addOrReplaceChild("bannerR", CubeListBuilder.create().texOffs(179, 67).addBox(-5.5F, -2.0F, -0.5F, 10.0F, 4.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(92, 156).addBox(-4.5F, 2.0F, -0.5F, 9.0F, 24.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(187, 50).addBox(-3.5F, 26.0F, -0.5F, 8.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-28.5F, -48.0F, 0.0F));
 
 		PartDefinition chains = partdefinition.addOrReplaceChild("chains", CubeListBuilder.create().texOffs(29, 157).addBox(14.1F, -58.0F, -0.5F, 1.0F, 13.0F, 1.0F, new CubeDeformation(0.0F))
 				.texOffs(34, 169).addBox(-14.0F, -58.0F, -0.5F, 1.0F, 26.0F, 1.0F, new CubeDeformation(0.0F))
@@ -170,8 +156,28 @@ public class HematicArmatureModel extends Model {
 				.texOffs(138, 182).addBox(4.0F, -4.0F, -4.0F, 1.0F, 1.0F, 8.0F, new CubeDeformation(0.0F))
 				.texOffs(0, 183).addBox(-4.5F, -4.0F, -4.0F, 1.0F, 1.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(-29.0F, -10.0F, 17.0F));
 
+		PartDefinition vicarkit_upgrade = partdefinition.addOrReplaceChild("vicarkit_upgrade", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition candles = vicarkit_upgrade.addOrReplaceChild("candles", CubeListBuilder.create().texOffs(192, 93).addBox(-18.0F, -2.0F, 17.3333F, 2.0F, 5.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(39, 186).addBox(-16.0F, -5.0F, -8.6667F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(96, 192).addBox(-9.0F, -1.0F, -11.6667F, 2.0F, 4.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(192, 84).addBox(7.0F, -3.0F, -11.6667F, 2.0F, 6.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(127, 183).addBox(14.0F, -5.0F, -8.6667F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F))
+				.texOffs(87, 192).addBox(16.0F, -2.0F, 17.3333F, 2.0F, 5.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -3.0F, 6.6667F, 0.0F, 3.1416F, 0.0F));
+
+		PartDefinition banners = vicarkit_upgrade.addOrReplaceChild("banners", CubeListBuilder.create(), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
+
+		PartDefinition bannerL = banners.addOrReplaceChild("bannerL", CubeListBuilder.create().texOffs(179, 61).addBox(-4.5F, -2.0F, -0.5F, 10.0F, 4.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(151, 114).addBox(-4.5F, 2.0F, -0.5F, 9.0F, 24.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(187, 44).addBox(-4.5F, 26.0F, -0.5F, 8.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(28.5F, -48.0F, 0.0F));
+
+		PartDefinition bannerR = banners.addOrReplaceChild("bannerR", CubeListBuilder.create().texOffs(179, 67).addBox(-5.5F, -2.0F, -0.5F, 10.0F, 4.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(92, 156).addBox(-4.5F, 2.0F, -0.5F, 9.0F, 24.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(187, 50).addBox(-3.5F, 26.0F, -0.5F, 8.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-28.5F, -48.0F, 0.0F));
+
 		return LayerDefinition.create(meshdefinition, 256, 256);
 	}
+
 	public void setupBannerAnimation(float ageInTicks) {
 		bannerL.resetPose();
 		bannerR.resetPose();
@@ -189,11 +195,11 @@ public class HematicArmatureModel extends Model {
 		left_pillar.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
 		right_pillar.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
 		arch.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
-		apex.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
+		monolithic_cornerstone.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
 		left_vial.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
 		right_vial.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
-		banners.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
 		chains.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
 		bowls.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
+		vicarkit_upgrade.render(poseStack, vertexConsumer, packedLight, packedOverlay, packedColor);
 	}
 }
